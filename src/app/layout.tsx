1eb6cbd626ff86c6b80ff63380f9a4d5896708f1
Sudhanshu Bhagwat <sudhanshubhagwat3@gmail.com>
2025-05-18 23:50:47 +0530

    Testing viewport changes for nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export async function generateMetadata() {
   return {
-    title: "Sudhanshu Bhagat",
+    title: "Sudhanshu Bhagwat",
     description: "A software developer, here for the fun of it",
     other: {
       "apple-mobile-web-app-status-bar-style": "default",
@@ -20,6 +20,10 @@ export const viewport: Viewport = {
     { media: "(prefers-color-scheme: light)", color: "#f9fafb" },
     { media: "(prefers-color-scheme: dark)", color: "#09090b" },
   ],
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  width: "device-width",
 };
 
 export default function RootLayout({
